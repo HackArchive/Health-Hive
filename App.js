790826884b1316446bd2917ed86e7e6ac08baf18
AtmegaBuzz <swapnilshinde9382@gmail.com>
2023-01-07 00:38:52 +0530

    analyse feature

--- App.js
+++ App.js
@@ -1,68 +1,58 @@
-import { useState, createContext } from "react";
-import { StatusBar } from "expo-status-bar";
-import { BottomNavigation, Text, Provider } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useState, createContext} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { BottomNavigation, Text, Provider} from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import Contribute from "./src/routes/Contribute";
-import Home from "./src/routes/Home";
-import ProductDetail from "./src/routes/ProductDetail";
-import AuthStack from "./src/routes/AuthStack";
+import Contribute from './src/routes/Contribute';
+import Home from './src/routes/Home';
+import Analyse from './src/routes/Analyse';
+import ProductDetail from './src/routes/ProductDetail';
 
 const Context = createContext();
 
 export default function App() {
+  
+  
   const [index, setIndex] = useState(0);
-  const [barcode, setBarcode] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
+  const [barcode,setBarcode] = useState('');
+  const [searchQuery,setSearchQuery] = useState('');
 
   const contextValue = {
     barcode,
     searchQuery,
 
     setBarcode,
-    setSearchQuery,
-  };
+    setSearchQuery
+  }
 
   const [routes] = useState([
-    {
-      key: "home",
-      title: "Home",
-      focusedIcon: "home",
-      unfocusedIcon: "home-outline",
-    },
-    { key: "contribute", title: "Contribute", focusedIcon: "plus" },
-    {
-      key: "analyse",
-      title: "Analyse",
-      focusedIcon: "food",
-      unfocusedIcon: "food-outline",
-    },
+    { key: 'home', title: 'Home', focusedIcon: 'home', unfocusedIcon: 'home-outline'},
+    { key: 'contribute', title: 'Contribute', focusedIcon: 'plus' },
+    { key: 'analyse', title: 'Analyse', focusedIcon: 'food', unfocusedIcon: 'food-outline' },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     home: Home,
     contribute: Contribute,
     analyse: Analyse,
-    productDetail: ProductDetail,
+    productDetail:ProductDetail
   });
 
   return (
+
     <Context.Provider value={contextValue}>
       <SafeAreaProvider>
         <Provider>
-          <NavigationContainer>
-            <AuthStack />
-          </NavigationContainer>
-          {/* <BottomNavigation
+          <BottomNavigation
             navigationState={{ index, routes }}
             onIndexChange={setIndex}
             renderScene={renderScene}
-          /> */}
+          />
         </Provider>
       </SafeAreaProvider>
     </Context.Provider>
-  );
-}
 
-export { Context };
+  );
+};
+
+export {Context};
