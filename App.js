5f0664586d7af0154b50851763f3d35433b44cb4
vishal-codes <itsvishal2417@gmail.com>
2023-01-07 17:49:48 +0530

    Fixed header screen of auth screens
    Signed-off-by: vishal-codes <itsvishal2417@gmail.com>

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
-import { useState, createContext} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { BottomNavigation, Text, Provider} from 'react-native-paper';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useState, createContext } from "react";
+import { StatusBar } from "expo-status-bar";
+import { BottomNavigation, Text, Provider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-
-import Contribute from './src/routes/Contribute';
-import Home from './src/routes/Home';
-import Analyse from './src/routes/Analyse';
-import ProductDetail from './src/routes/ProductDetail';
+import Contribute from "./src/routes/Contribute";
+import Home from "./src/routes/Home";
+import Analyse from "./src/routes/Analyse";
+import ProductDetail from "./src/routes/ProductDetail";
 import LoginScreen from "./src/routes/LoginScreen";
 import RegisterScreen from "./src/routes/RegisterScreen";
-import Profile from './src/routes/Profile';
+import Profile from "./src/routes/Profile";
 
 const Context = createContext();
 const AuthStack = createStackNavigator();
 
-
 export default function App() {
-  
-  
   const [index, setIndex] = useState(0);
-  const [barcode,setBarcode] = useState('');
-  const [searchQuery,setSearchQuery] = useState('');
-  const [isLoggedIn,setIsLoggedIn] = useState(false);
-  const [userDetails,setUserDetails] = useState(null);
-  const [products,setProducts] = useState([]);
+  const [barcode, setBarcode] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userDetails, setUserDetails] = useState(null);
+  const [products, setProducts] = useState([]);
 
   const contextValue = {
     barcode,
@@ -39,41 +35,49 @@ export default function App() {
     setBarcode,
     setSearchQuery,
     setIsLoggedIn,
-    setUserDetails
-  }
+    setUserDetails,
+  };
 
   const [routes] = useState([
-    { key: 'home', title: 'Home', focusedIcon: 'home', unfocusedIcon: 'home-outline'},
-    { key: 'contribute', title: 'Contribute', focusedIcon: 'plus' },
-    { key: 'analyse', title: 'Analyse', focusedIcon: 'food', unfocusedIcon: 'food-outline' },
-    { key: 'profile', title: 'Profile', focusedIcon: 'account', unfocusedIcon: 'account'}
+    {
+      key: "home",
+      title: "Home",
+      focusedIcon: "home",
+      unfocusedIcon: "home-outline",
+    },
+    { key: "contribute", title: "Contribute", focusedIcon: "plus" },
+    {
+      key: "analyse",
+      title: "Analyse",
+      focusedIcon: "food",
+      unfocusedIcon: "food-outline",
+    },
+    {
+      key: "profile",
+      title: "Profile",
+      focusedIcon: "account",
+      unfocusedIcon: "account",
+    },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     home: Home,
     contribute: Contribute,
     analyse: Analyse,
-    productDetail:ProductDetail,
-    profile:Profile
+    productDetail: ProductDetail,
+    profile: Profile,
   });
 
-
-
   return (
-
-
-
     <Context.Provider value={contextValue}>
-      {
-        isLoggedIn ?
+      {!isLoggedIn ? (
         <NavigationContainer>
-        <AuthStack.Navigator>
-          <AuthStack.Screen name="Login" component={LoginScreen} />
-          <AuthStack.Screen name="Register" component={RegisterScreen} />
-        </AuthStack.Navigator>
-      </NavigationContainer>
-
-        :
+          <AuthStack.Navigator screenOptions={{ headerShown: false }}>
+            <AuthStack.Screen name="Login" component={LoginScreen} />
+            <AuthStack.Screen name="Register" component={RegisterScreen} />
+          </AuthStack.Navigator>
+        </NavigationContainer>
+      ) : (
         <SafeAreaProvider>
           <Provider>
             <BottomNavigation
@@ -83,10 +87,9 @@ export default function App() {
             />
           </Provider>
         </SafeAreaProvider>
-      }
+      )}
     </Context.Provider>
-
   );
-};
+}
 
-export {Context};
+export { Context };
